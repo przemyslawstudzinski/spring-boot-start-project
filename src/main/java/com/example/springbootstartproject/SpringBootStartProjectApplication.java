c8d947fdcc7b8e971c1039005507c997e1b95792
Przemysław Studziński <przemyslawstudzinski1993@gmail.com>
2018-04-23 21:24:07 +0200

    Initial commit

---
+++ src/main/java/com/example/springbootstartproject/SpringBootStartProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootstartproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStartProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStartProjectApplication.class, args);
+	}
+}
